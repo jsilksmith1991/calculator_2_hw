237a3a568ae3d108a1b0575c98a61ae8e7858839
YOUR NAME <jsilksmith@Jeremys-MacBook-Pro.local>
2016-02-29 18:00:39 -0500

    firt commit

---
+++ javaScript.js
@@ -0,0 +1,27 @@
+function add(){
+  var x = document.getElementById("numberOne").value;
+  var y = document.getElementById("numberTwo").value;
+  var answer = Number(x) + Number(y);
+  alert(answer);
+}
+
+function multiply(){
+  var x = document.getElementById("numberOne").value;
+  var y = document.getElementById("numberTwo").value;
+  var answer = Number(x) * Number(y);
+  alert(answer);
+}
+
+function subtract(){
+  var x = document.getElementById("numberOne").value;
+  var y = document.getElementById("numberTwo").value;
+  var answer = Number(x) - Number(y);
+  alert(answer);
+}
+
+function divide(){
+  var x = document.getElementById("numberOne").value;
+  var y = document.getElementById("numberTwo").value;
+  var answer = Number(x) / Number(y);
+  alert(answer);
+} 
